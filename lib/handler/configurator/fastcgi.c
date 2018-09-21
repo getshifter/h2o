5562a36eb71f5019b2d9e9d45b62b5c2e8264e16
sawanoboly <sawanoboriyu@higanworks.com>
2018-09-21 19:12:45 +0900

    add config fastcgi.chroot

--- lib/handler/configurator/fastcgi.c
+++ lib/handler/configurator/fastcgi.c
@@ -68,6 +68,23 @@ static int on_config_document_root(h2o_configurator_command_t *cmd, h2o_configur
     return 0;
 }
 
+static int on_config_chroot(h2o_configurator_command_t *cmd, h2o_configurator_context_t *ctx, yoml_t *node)
+{
+    struct fastcgi_configurator_t *self = (void *)cmd->configurator;
+
+    if (node->data.scalar[0] == '\0') {
+        /* unset */
+        self->vars->chroot = h2o_iovec_init(NULL, 0);
+    } else if (node->data.scalar[0] == '/') {
+        /* set */
+        self->vars->chroot = h2o_iovec_init(node->data.scalar, strlen(node->data.scalar));
+    } else {
+        h2o_configurator_errprintf(cmd, node, "value does not start from `/`");
+        return -1;
+    }
+    return 0;
+}
+
 static int on_config_send_delegated_uri(h2o_configurator_command_t *cmd, h2o_configurator_context_t *ctx, yoml_t *node)
 {
     struct fastcgi_configurator_t *self = (void *)cmd->configurator;
@@ -362,6 +379,9 @@ void h2o_fastcgi_register_configurator(h2o_globalconf_t *conf)
     h2o_configurator_define_command(&c->super, "fastcgi.document_root",
                                     H2O_CONFIGURATOR_FLAG_ALL_LEVELS | H2O_CONFIGURATOR_FLAG_EXPECT_SCALAR,
                                     on_config_document_root);
+    h2o_configurator_define_command(&c->super, "fastcgi.chroot",
+                                    H2O_CONFIGURATOR_FLAG_ALL_LEVELS | H2O_CONFIGURATOR_FLAG_EXPECT_SCALAR,
+                                    on_config_chroot);
     h2o_configurator_define_command(&c->super, "fastcgi.send-delegated-uri",
                                     H2O_CONFIGURATOR_FLAG_ALL_LEVELS | H2O_CONFIGURATOR_FLAG_EXPECT_SCALAR,
                                     on_config_send_delegated_uri);
